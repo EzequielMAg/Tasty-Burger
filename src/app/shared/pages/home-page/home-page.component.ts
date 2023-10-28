7738441a7fa27956335cdacb6182d5fab273af6e
Florencia <flordani98@gmail.com>
2023-10-28 17:42:24 -0300

    Agrega maquetación html y css en componente hero - Diseña logo de la aplicación - Agrega logo y estilos en componente navbar

--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-home-page',
   templateUrl: './home-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
 
