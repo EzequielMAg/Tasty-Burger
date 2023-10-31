703d14047ca05eaeb834ba534ba7d2f37e305216
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2023-10-31 02:20:28 -0300

    Refactoriza en interfaces y models - Implementa  uui() para que los id sean unicos - Ordena para mejorar legibilidad de los tipos de dato de los atributos - Compara contra tipo undefined cada atributo en el constructor

--- src/app/core/interfaces.ts
+++ src/app/core/interfaces.ts
@@ -1,35 +1,34 @@
 export interface IUser {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-    phoneNumber: number;
-    address: string;
+  id:          string | null;
+  name:        string;
+  email:       string;
+  password:    string;
+  phoneNumber: number;
+  address:     string;
 }
 
 export interface IProduct{
-    id: string;
-    name: string;
-    price: number;
-    description: string;
+  id:          string | null;
+  name:        string;
+  price:       number;
+  description: string;
 
-    category: Category;
-    urlImage: string;
+  category:    Category;
+  urlImage:    string;
 }
 
 export enum Category{
-    Burguers = "Hamburguesa",
-    SoftDrinks = "Bebidas sin alcohol",
-    Beers = "Cervezas",
-    Fries = "Frituras", 
-    Desserts = "Postres",
-    Salads = "Ensaladas",
-    BurguerVeggies = "Hamburguesas Veganas"
-    
+  Burguers = "Hamburguesa",
+  SoftDrinks = "Bebidas sin alcohol",
+  Beers = "Cervezas",
+  Fries = "Frituras",
+  Desserts = "Postres",
+  Salads = "Ensaladas",
+  BurguerVeggies = "Hamburguesas Veganas"
 }
 
 export interface ICart{
-    productList: IProduct[],
-    cartPrice: number,
-    payMode: string
+  productList: IProduct[],
+  cartPrice:   number,
+  payMode:     string
 }
--- src/app/core/models.ts
+++ src/app/core/models.ts
@@ -2,41 +2,37 @@ import { v4 as uuid} from 'uuid';
 import { Category, IProduct, IUser } from './interfaces';
 
 export class User implements IUser{
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-    phoneNumber: number;
-    address: string;
+  id:           string;
+  name:         string;
+  email:        string;
+  password:     string;
+  phoneNumber:  number;
+  address:      string;
 
-    constructor(user?: any){
-        this.id = user.id != null ? user.id : null;
-        this.name = user.name != null ? user.name : null;
-        this.email = user.email != null ? user.email : null;
-        this.password = user.password != null ? user.password : null;
-        this.phoneNumber = user.phoneNumber != null ? user.phoneNumber : null;
-        this.address = user.address != null ? user.address : null;
-    }
+  constructor(user?: any){
+    this.id = user.id === undefined ? uuid() : user.id ;
+    this.name = user.name === undefined ? null : user.name;
+    this.email = user.email === undefined ? null : user.email;
+    this.password = user.password === undefined ? null : user.password;
+    this.phoneNumber = user.phoneNumber === undefined ? null : user.phoneNumber;
+    this.address = user.address === undefined ? null : user.address;
+  }
 }
 
 export class Product implements IProduct{
+  id:           string;
+  name:         string;
+  price:        number;
+  description:  string;
+  category:     Category;
+  urlImage:     string;
 
-    id: string;
-    name: string;
-    price: number;
-    description: string;
-    category: Category;
-    urlImage: string;
-
-    constructor(product?: any){
-
-        // this.id = uuid();
-        this.id = product.id != null ? product.id : null;
-        this.name = product.name != null ? product.name : null;
-        this.price = product.price != null ? product.price : null;
-        this.description = product.description != null ? product.description : null;
-        this.urlImage = product.urlImage != null ? product.urlImage : null;
-        this.category = product.category != null ? product.category : null;
-    }
-    
+  constructor(product?: any){
+    this.id = product.id === undefined ? uuid() : product.id;
+    this.name = product.name === undefined ? null :  product.name;
+    this.price = product.price === undefined ? null : product.price;
+    this.description = product.description === undefined ? null : product.description;
+    this.urlImage = product.urlImage === undefined ? null : product.urlImage;
+    this.category = product.category === undefined ? null : product.category;
+  }
 }
