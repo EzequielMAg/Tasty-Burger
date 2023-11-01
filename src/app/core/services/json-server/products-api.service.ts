7a29aa4427599cea4bb1f83de096223e4f50f964
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2023-11-01 02:13:55 -0300

    Refactoriza apiService Modulariza metodos de peticiones HTTP a json server en: - ProductsApiService - UsersApiService
    Implmementa metodo getProducts en MenuPageComponent

---
+++ src/app/core/services/json-server/products-api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map, of } from 'rxjs';
+import { Product } from '../../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsApiService {
+
+  public baseURL: string = 'http://localhost:3000/products';
+
+  constructor(private http: HttpClient) { }
+
+  public getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseURL}`);
+  }
+
+  public addProduct(product: Product): Observable<boolean> {
+    return this.http.post<boolean>(`${this.baseURL}`, product);
+  }
+
+  public deleteProduct(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.baseURL}${id}`)
+    .pipe(
+      map( resp => true ),  // Si sale bien retorna true. Recibir un response significa que salio bien
+      catchError( error => of(false)) // Si hay algun error en la solicitud me regresa falso
+    );
+  }
+
+  public updateProduct(product: Product): Observable<boolean> {
+    return this.http.patch<boolean>(`${this.baseURL}/${product.id}`, product);
+  }
+}
--- src/app/modules/products/pages/menu-page/menu-page.component.ts
+++ src/app/modules/products/pages/menu-page/menu-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { Product } from 'src/app/core/models';
+import { ProductsApiService } from 'src/app/core/services/json-server/products-api.service';
 
 @Component({
   selector: 'products-menu-page',
   templateUrl: './menu-page.component.html',
   styleUrls: [ './menu-page.component.css' ]
 })
-export class MenuPageComponent {
+export class MenuPageComponent implements OnInit {
+
+  constructor(private productsApiService: ProductsApiService) {}
+
+  ngOnInit(): void {
+
+    this.getProducts();
+  }
+
+  public products: Product[] = [];
+
+  public async getProducts() {
+    try {
+      let responseApi = this.productsApiService.getProducts();
+      const data = await lastValueFrom(responseApi);
+
+      this.products = data.map( (productData: any) => new Product(productData) );
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
 
 }
