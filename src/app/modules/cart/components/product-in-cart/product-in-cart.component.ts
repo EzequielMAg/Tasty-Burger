522acebaef584cd9c79ffa1973bedc2070247c2f
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2023-11-05 03:25:42 -0300

    Agrega componentes ListProductsCartComponent y ProductInCartComponent

---
+++ src/app/modules/cart/components/product-in-cart/product-in-cart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-in-cart',
+  templateUrl: './product-in-cart.component.html',
+  styleUrls: ['./product-in-cart.component.css']
+})
+export class ProductInCartComponent {
+
+}
