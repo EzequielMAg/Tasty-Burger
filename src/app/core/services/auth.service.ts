a41d8c72d38a718a4be4fe21e6a5eb85f47fd359
Florencia <flordani98@gmail.com>
2023-11-05 02:49:51 -0300

    Implementa registro de usuarios - Agrega metodo petición http para añadir usuarios al db.json - Maquetación html y css en el componente registro

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -52,4 +52,13 @@ export class AuthService {
     return localStorage.getItem('token') ? true : false;
   }
 
+  public addUser(user: User){
+    return new Promise<User>((resolve, reject) =>{
+        this.usersApiService.addUser(user).subscribe({
+            next: data => resolve(data),
+            error: error => reject(error)
+        })
+    });
+}
+
 }
--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/core/models';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['register.component.css']
 })
 export class RegisterComponent {
+
+  public user: User = new User({id:null});
+  public passwordConfirm: string = '';
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  public registerUser(){
+    this.authService.addUser(this.user);
+    this.router.navigate(["/products"]);
+    console.log(this.user);
+  }
 
 }
