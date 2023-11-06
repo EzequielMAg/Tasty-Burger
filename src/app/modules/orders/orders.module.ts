4b9fa61373d926fb2ea31cf68608f5cc9305c52d
Florencia <flordani98@gmail.com>
2023-11-06 13:33:38 -0300

    Implementa vista de pedidos realizados
    -Maquetación html y css para la vista de pedidos

--- src/app/modules/orders/orders.module.ts
+++ src/app/modules/orders/orders.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OrderCardComponent } from './components/order-card/order-card.component';
-import { OrderCardListComponent } from './components/order-card-list/order-card-list.component';
 import { OrdersPageComponent } from './pages/orders-page/orders-page.component';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { OrdersListComponent } from './components/orders-list/orders-list.component';
+import { OrderItemComponent } from './components/order-item/order-item.component';
 
 
 
 @NgModule({
   declarations: [
-    OrderCardComponent,
-    OrderCardListComponent,
-    OrdersPageComponent
+    OrderItemComponent,
+    OrdersListComponent,
+    OrdersPageComponent,
   ],
   imports: [
     CommonModule,
--- src/app/modules/orders/pages/orders-page/orders-page.component.ts
+++ src/app/modules/orders/pages/orders-page/orders-page.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { Category } from 'src/app/core/interfaces';
+import { Product } from 'src/app/core/models';
+import { ProductsApiService } from 'src/app/core/services/json-server/products-api.service';
 
 @Component({
   selector: 'app-orders-page',
   templateUrl: './orders-page.component.html',
-  styles: [`.add-margin {
-    margin: 20rem auto;
-  }
-  `
-  ]
+  styleUrls: ['./orders-page.component.css']
 })
-export class OrdersPageComponent {
+export class OrdersPageComponent implements OnInit{
+
+  public orders: Product[] = [];
+
+  constructor(private productsApiService: ProductsApiService){}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+
+
+
+  //*pruebo con productos, pero en realidad tendria q tener un metodo get para traer pedidos
+
+  public async getProducts() {
+    try {
+      let responseApi = this.productsApiService.getProductsByCategory(Category.Burguers);
+      const data = await lastValueFrom(responseApi);
+
+      this.orders = data.map( (productData: any) => new Product(productData) );
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  //*realizar metodo para traer los productos q se encuentran en el pedido, Class: ProductInCart
 
 }
