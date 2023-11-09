2bbc70df2861ba3d35ed2bc5016ae11c2be8f807
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2023-11-08 23:52:53 -0300

    Agrega metodo calculateTotalToPay() en ListProductsCartComponent

--- src/app/modules/cart/components/list-products-cart/list-products-cart.component.ts
+++ src/app/modules/cart/components/list-products-cart/list-products-cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ProductInCart } from 'src/app/core/models';
+import { Product } from '../../../../core/models';
 
 @Component({
   selector: 'list-products-cart',
@@ -11,5 +12,19 @@ export class ListProductsCartComponent {
   @Input()
   public cart: ProductInCart[] = [];
 
+
+  public calculateTotalToPay() {
+
+    let totalToPay = this.cart.reduce( (total, productInCart) => {
+
+      const productPrice: number = productInCart.product.price;
+      const quantity: number = productInCart.quantity;
+      const subtotal: number = productPrice * quantity;
+
+      return total + subtotal;
+    }, 0);
+
+    return totalToPay;
+  }
 
 }
