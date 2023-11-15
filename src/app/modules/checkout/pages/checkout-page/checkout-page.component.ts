f437df9aa71db623b923d2eb48d8f6a32ccc1d8a
Florencia <flordani98@gmail.com>
2023-11-15 05:20:33 -0300

    Finaliza vista de componente checkout page

--- src/app/modules/checkout/pages/checkout-page/checkout-page.component.ts
+++ src/app/modules/checkout/pages/checkout-page/checkout-page.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EditAddressComponent } from '../../components/edit-address/edit-address.component';
+import { EditPayModeComponent } from '../../components/edit-pay-mode/edit-pay-mode.component';
+import { DeliveryType, PayMode } from 'src/app/core/enums';
+import { EditDeliveryTypeComponent } from '../../components/edit-delivery-type/edit-delivery-type.component';
+import { SendOrderDialogComponent } from '../../components/send-order-dialog/send-order-dialog.component';
 
 @Component({
   selector: 'app-checkout-page',
@@ -6,5 +12,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout-page.component.css']
 })
 export class CheckoutPageComponent {
+
+  public address: string = 'Ingresa tu dirección';
+  public payMode: PayMode = PayMode.withoutPaymentMethod;
+  public deliveryType: DeliveryType = DeliveryType.Delivery;
+
+  constructor(private dialog: MatDialog){}
+
+  editAddress(){
+    const dialogRef = this.dialog.open(EditAddressComponent, {data: this.address, height: '220px', width: '300px'});
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("el cuadro de dialogo se cerro con resultado: ", result);
+    })
+  }
+
+  editDeliveryType(){
+    const dialogRef = this.dialog.open(EditDeliveryTypeComponent, {data: this.deliveryType, height: '220px', width: '300px'});
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("el cuadro de dialogo se cerro con resultado: ", result);
+    console.log(this.deliveryType);
+  })
+}
+
+  editPayMode(){
+    const dialogRef = this.dialog.open(EditPayModeComponent, {data: this.payMode, height: '400px', width: '350px'});
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("el cuadro de dialogo se cerro con resultado: ", result);
+    console.log(this.payMode);
+
+    })
+  }
+
+  onSubmit(){
+    const dialogRef = this.dialog.open(SendOrderDialogComponent, {height: '280', width: '400'});
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("el cuadro de dialogo se cerro con resultado: ", result);
+
+    })
+
+  }
+
+
 
 }
