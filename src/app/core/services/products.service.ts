399e2cb3ab517bab5f1de1c8162b2115ce6d4735
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2023-11-14 22:33:47 -0300

    Refactoriza ProductsPageComponent y agrega ProductsService - Se mueve toda la data y metodos del componente al nuevo servicio - En el servicio:    -Se hacen privados a todos los arreglos de productos y se crean sus getters    -Se crea el metodo getProductById() - En el componente:    -Se crean los getters para acceder a los arreglos de productos que pasaron todos al servicio

---
+++ src/app/core/services/products.service.ts
@@ -0,0 +1,132 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models';
+import { Category } from '../enums';
+import { ProductsApiService } from './json-server/products-api.service';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private _productsBurguers: Product[] = [];
+  private _productsBurguerVeggies: Product[] = [];
+  private _productsFries: Product[] = [];
+  private _productsSalads: Product[] = [];
+  private _productsSoftDrinks: Product[] = [];
+  private _productsBeers: Product[] = [];
+  private _productsDesserts: Product[] = [];
+
+  private _loadedProducts: boolean = false;
+
+  constructor(private productsApiService: ProductsApiService) { }
+
+  //#region GETTERS
+  get loadedProducts(): boolean {
+    return this._loadedProducts;
+  }
+
+  get productsBurguers(): Product[] {
+    return this._productsBurguers;
+  }
+
+  get productsBurguerVeggies(): Product[] {
+    return this._productsBurguerVeggies;
+  }
+
+  get productsFries(): Product[] {
+    return this._productsFries;
+  }
+
+  get productsSalads(): Product[] {
+    return this._productsSalads;
+  }
+
+  get productsSoftDrinks(): Product[] {
+    return this._productsSoftDrinks;
+  }
+
+  get productsBeers(): Product[] {
+    return this._productsBeers;
+  }
+
+  get productsDesserts(): Product[] {
+    return this._productsDesserts;
+  }
+  //#endregion
+
+  public getProducts() {
+    this.getProductsByCategory(Category.Burguers);
+    this.getProductsByCategory(Category.BurguerVeggies);
+    this.getProductsByCategory(Category.Fries);
+    this.getProductsByCategory(Category.Salads);
+    this.getProductsByCategory(Category.SoftDrinks);
+    this.getProductsByCategory(Category.Beers);
+    this.getProductsByCategory(Category.Desserts);
+
+    this._loadedProducts = true;
+  }
+
+  public async getProductsByCategory(category : Category) {
+    try {
+      let responseApi = this.productsApiService.getProductsByCategory(category);
+      const data = await lastValueFrom(responseApi);
+
+      switch(category){
+        case Category.Burguers:
+          this._productsBurguers = data.map( (productData: any) => new Product(productData) );
+          break;
+        case Category.BurguerVeggies:
+          this._productsBurguerVeggies = data.map( (productData: any) => new Product(productData) );
+          break;
+        case Category.Fries:
+          this._productsFries = data.map( (productData: any) => new Product(productData) );
+          break;
+        case Category.Salads:
+          this._productsSalads = data.map( (productData: any) => new Product(productData) );
+          break;
+        case Category.SoftDrinks:
+          this._productsSoftDrinks = data.map( (productData: any) => new Product(productData) );
+          break;
+        case Category.Beers:
+          this._productsBeers = data.map( (productData: any) => new Product(productData) );
+          break;
+        case Category.Desserts:
+          this._productsDesserts = data.map( (productData: any) => new Product(productData) );
+          break;
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public getProductById(idProduct: string):Product | undefined {
+
+    let product: Product | undefined;
+
+    //Recorrer todos los arreglos hasta encontrar el producto
+    product = this._productsBurguers.find((p) => p.id === idProduct);
+    if (product) return product;
+
+    product = this._productsBurguerVeggies.find((p) => p.id === idProduct);
+    if (product) return product;
+
+    product = this._productsFries.find((p) => p.id === idProduct);
+    if (product) return product;
+
+    product = this._productsSalads.find((p) => p.id === idProduct);
+    if (product) return product;
+
+    product = this._productsSoftDrinks.find((p) => p.id === idProduct);
+    if (product) return product;
+
+    product = this._productsBeers.find((p) => p.id === idProduct);
+    if (product) return product;
+
+    product = this._productsDesserts.find((p) => p.id === idProduct);
+    if (product) return product;
+
+    return product;
+  }
+}
--- src/app/modules/products/pages/menu-page/menu-page.component.ts
+++ src/app/modules/products/pages/menu-page/menu-page.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
-import { Category } from 'src/app/core/enums';
 import { Product } from 'src/app/core/models';
-import { ProductsApiService } from 'src/app/core/services/json-server/products-api.service';
+import { ProductsService } from 'src/app/core/services/products.service';
 
 @Component({
   selector: 'products-menu-page',
@@ -11,67 +9,42 @@ import { ProductsApiService } from 'src/app/core/services/json-server/products-a
 })
 export class MenuPageComponent implements OnInit {
 
-  public productsBurguers: Product[] = [];
-  public productsBurguerVeggies: Product[] = [];
-  public productsFries: Product[] = [];
-  public productsSalads: Product[] = [];
-  public productsSoftDrinks: Product[] = [];
-  public productsBeers: Product[] = [];
-  public productsDesserts: Product[] = [];
-
-  private loadedProducts: boolean = false;
-
-  constructor(private productsApiService: ProductsApiService) {}
+  constructor(private productService: ProductsService) {}
 
   ngOnInit(): void {
 
-    if(!this.loadedProducts)
-      this.getProducts();
+    if(!this.productService.loadedProducts)
+      this.productService.getProducts();
   }
 
-  public async getProducts() {
-    this.getProductsByCategory(Category.Burguers);
-    this.getProductsByCategory(Category.BurguerVeggies);
-    this.getProductsByCategory(Category.Fries);
-    this.getProductsByCategory(Category.Salads);
-    this.getProductsByCategory(Category.SoftDrinks);
-    this.getProductsByCategory(Category.Beers);
-    this.getProductsByCategory(Category.Desserts);
-
-    this.loadedProducts = true;
+  //#region GETTERS
+  get productsBurguers(): Product[] {
+    return this.productService.productsBurguers;
   }
 
-  public async getProductsByCategory(category : Category) {
-    try {
-      let responseApi = this.productsApiService.getProductsByCategory(category);
-      const data = await lastValueFrom(responseApi);
-
-      switch(category){
-        case Category.Burguers:
-          this.productsBurguers = data.map( (productData: any) => new Product(productData) );
-          break;
-        case Category.BurguerVeggies:
-          this.productsBurguerVeggies = data.map( (productData: any) => new Product(productData) );
-          break;
-        case Category.Fries:
-          this.productsFries = data.map( (productData: any) => new Product(productData) );
-          break;
-        case Category.Salads:
-          this.productsSalads = data.map( (productData: any) => new Product(productData) );
-          break;
-        case Category.SoftDrinks:
-          this.productsSoftDrinks = data.map( (productData: any) => new Product(productData) );
-          break;
-        case Category.Beers:
-          this.productsBeers = data.map( (productData: any) => new Product(productData) );
-          break;
-        case Category.Desserts:
-          this.productsDesserts = data.map( (productData: any) => new Product(productData) );
-          break;
-      }
-
-    } catch (error) {
-      console.log(error);
-    }
+  get productsBurguerVeggies(): Product[] {
+    return this.productService.productsBurguerVeggies;
   }
+
+  get productsFries(): Product[] {
+    return this.productService.productsFries;
+  }
+
+  get productsSalads(): Product[] {
+    return this.productService.productsSalads;
+  }
+
+  get productsSoftDrinks(): Product[] {
+    return this.productService.productsSoftDrinks;
+  }
+
+  get productsBeers(): Product[] {
+    return this.productService.productsBeers;
+  }
+
+  get productsDesserts(): Product[] {
+    return this.productService.productsDesserts;
+  }
+  //#endregion
+
 }
