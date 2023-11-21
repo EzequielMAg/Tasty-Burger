9f106c43d7c11d197ffa4c53214bf6c9acc8a661
Florencia <flordani98@gmail.com>
2023-11-20 23:47:21 -0300

    Corrige redireccionamiento en product-in-cart component
    -Implementa estilos segun path /cart

--- src/app/modules/cart/components/product-in-cart/product-in-cart.component.ts
+++ src/app/modules/cart/components/product-in-cart/product-in-cart.component.ts
@@ -2,23 +2,23 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Product, ProductLine } from 'src/app/core/models';
 import { CartService } from '../../../../core/services/cart.service';
 import { ProductsService } from 'src/app/core/services/products.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'product-in-cart',
   templateUrl: './product-in-cart.component.html',
   styleUrls: ['./product-in-cart.component.css']
 })
-export class ProductInCartComponent implements OnInit {
+export class ProductInCartComponent{
 
   @Input()
   public productInCart!: ProductLine;
 
-  constructor(private cartService: CartService, private productService: ProductsService) {}
+  constructor(private cartService: CartService, 
+              private productService: ProductsService, 
+              private router: Router) {}
 
-  ngOnInit(): void {
-    if( !this.productInCart ) throw new Error('productLine property is required');
-  }
-
+  
   get product(): Product {
     return this.productService.getProductById(this.productInCart.idProduct)!;
   }
@@ -31,6 +31,10 @@ export class ProductInCartComponent implements OnInit {
 
     // Y AGREGA EL OBJETO CREADO ProductLine EN EL CARRITO, a traves del CartService
     this.cartService.updateProductFromCart(this.productInCart);
+  }
+
+  public isCartView(): boolean {
+    return this.router.url === '/cart';
   }
 
 }
